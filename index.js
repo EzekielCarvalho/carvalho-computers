c9cd42fec58eee21e7d84d8bc178c47a1fe901de
Ezekiel Carvalho <62009388+EzekielCarvalho@users.noreply.github.com>
2020-08-08 21:39:14 +0530

    make it better

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const port = process.env.PORT || 8080;
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
 const laptopData = JSON.parse(json);
@@ -62,9 +63,9 @@ const server = http.createServer((req, res) => {
 
 });
 
-server.listen(1337, '127.0.0.1', () => {
+/*server.listen(1337, '127.0.0.1', () => {
     console.log('Listening for requests now');
-});
+});*/
 
 function replaceTemplate(originalHtml, laptop) {
     let output = originalHtml.replace(/{%PRODUCTNAME%}/g, laptop.productName);
